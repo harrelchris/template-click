f2e1e0d317c515216f265c35b0e864e834f0e527
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-01-11 12:03:03 -0600

    black

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='app',
-    version='0.1.0',
-    py_modules=['app'],
+    name="app",
+    version="0.1.0",
+    py_modules=["app"],
     install_requires=[
-        'Click',
+        "Click",
     ],
     entry_points={
-        'console_scripts': [
-            'app = app:cli',
+        "console_scripts": [
+            "app = app:cli",
         ],
     },
 )
